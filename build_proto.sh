443665bd531daabc82c2277d5fa2c9f76b01a9e3
Geert Custers <geert.aj.custers@gmail.com>
2020-06-12 10:03:18 +0200

    Update proto submodule

--- build_proto.sh
+++ build_proto.sh
@@ -2,3 +2,4 @@
 
 protoc -I=proto/core --python_out=. proto/core/message.proto
 protoc -I=proto/light --python_out=. proto/light/light.proto
+protoc -I=proto/build -I=proto/ --python_out=. proto/build/build.proto
